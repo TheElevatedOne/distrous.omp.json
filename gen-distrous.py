fc7801ac267a06d05689c1fba06650ef8f94f268
TheElevatedOne <adam.mlady.7324@gmail.com>
2025-03-08 21:44:42 +0100

    added EOS option

---
+++ gen-distrous.py
@@ -0,0 +1,46 @@
+import json as js
+import os.path as op
+
+
+def main() -> None:
+    file_dir = op.dirname(op.expanduser(__file__))
+    config = op.join(file_dir, "config.json")
+
+    with open(config) as r:
+        cfg_dict = js.load(r)
+
+    for key, val in cfg_dict.items():
+        result = open(
+            op.join(
+                file_dir,
+                "themes",
+                f"distrous-{key.lower().replace(' ', '-')}.omp.json",
+            ),
+            "w",
+        )
+        with open(op.join(file_dir, "template.omp.json"), "r") as file:
+            for line in file.readlines():
+                result.write(
+                    line.replace(
+                        "%icon_properties",
+                        str(val["icon_properties"])[1:-1].replace("'", '"'),
+                    )
+                    .replace("%prompt_arr", val["prompt_arr"])
+                    .replace("%distro_bg", val["distro_bg"])
+                    .replace("%distro_fg", val["distro_fg"])
+                    .replace("%shell_bg", val["shell_bg"])
+                    .replace("%shell_fg", val["shell_fg"])
+                    .replace("%root_bg", val["root_bg"])
+                    .replace("%root_fg", val["root_fg"])
+                    .replace("%pipe", val["pipe"])
+                    .replace("%time", val["time"])
+                    .replace("%date", val["date"])
+                    .replace("%user", val["user"])
+                    .replace("%host", val["host"])
+                    .replace("%writable_dir", val["writable_dir"])
+                    .replace("%unwritable_dir", val["unwritable_dir"])
+                )
+
+
+if __name__ == "__main__":
+    main()
