03c151b289030b5db2a475860ca048ad49a119f8
TheElevatedOne <adam.mlady.7324@gmail.com>
2025-03-08 22:08:07 +0100

    added oh-my-posh previews generate script

---
+++ gen-previews.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Git Directory
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+# Colors
+RED='\033[0;31m'
+BLUE='\033[1;34m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+if ! command -v oh-my-posh 2>&1 >/dev/null; then
+  printf "${RED}[ERROR]${NC} ${BLUE}oh-my-posh${NC} binary not found / not on \$PATH.\nExiting...\n\n"
+  exit 1
+fi
+
+for theme in $(ls $SCRIPT_DIR/themes/); do
+  printf "${GREEN}[Generating Preview]${NC} $theme\n"
+  oh-my-posh --config "$SCRIPT_DIR/themes/$theme" config export image --author ELEVATED --background-color "#0d0f18" --output "$SCRIPT_DIR/previews/$theme.png"
+done
+
+printf "\n"
